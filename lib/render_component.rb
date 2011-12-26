0e6271fb40a808297b404b2e136b7813a13418cf
Yoshimoto <toshi@millefiori.co.jp>
2011-12-26 19:50:46 +0900

    initializing on load active_controller

--- lib/render_component.rb
+++ lib/render_component.rb
@@ -1,4 +1,6 @@
 require 'render_component/components'
 require 'action_controller'
 require 'action_dispatch/middleware/flash'
-ActionController::Base.send :include, RenderComponent::Components
+ActiveSupport.on_load(:action_controller) do
+  ActionController::Base.send :include, RenderComponent::Components
+end
